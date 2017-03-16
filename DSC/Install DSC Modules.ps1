64296c32b5c3afd3c30f2570e97add7e9a82c306
Robert Wakefield <Robert Wakefield>
2017-03-16 15:08:22 -0500

    Adding files

---
+++ DSC/Install DSC Modules.ps1
@@ -0,0 +1,9 @@
+﻿    Install-Module xPSDesiredStateConfiguration
+    Install-Module xPendingReboot
+    Install-Module xDSCDomainjoin -RequiredVersion 1.1
+    Install-Module xWebAdministration
+    Install-Module xWindowsUpdate
+    Install-Module xStorage
+    Install-Module xNetworking
+    Install-Module xActiveDirectory
+    Install-Module xComputerManagement
---
+++ DSC/MemberServer.ps1
@@ -0,0 +1,57 @@
+﻿Configuration MemberServer
+{
+    #Import Modules!
+    Import-DscResource -Module xPSDesiredStateConfiguration
+    Import-DscResource -Module xPendingReboot
+    Import-DscResource -Module xDSCDomainjoin -ModuleVersion 1.1
+    Import-DscResource -Module xWebAdministration
+    Import-DscResource -Module xWindowsUpdate
+    Import-DscResource -Module xStorage
+    Import-DscResource -Module xNetworking
+    Import-DscResource -Module xActiveDirectory
+    Import-DscResource -Module xComputerManagement
+
+    $SourceDir = 'D:\Source'
+
+    $WorkspaceID = Get-AutomationVariable -Name "_Global-WorkspaceID"
+    $DomainJoinCredentialName = Get-AutomationVariable -Name "_Global-DomainJoinCredentialName"
+    $DomainName = Get-AutomationVariable -Name "_Global-DomainName"
+
+    $DomainJoinCredential = Get-AutomationPSCredential -Name $DomainJoinCredentialName
+    $WorkspaceCredential = Get-AutomationPSCredential -Name $WorkspaceID
+    $WorkspaceKey = $WorkspaceCredential.GetNetworkCredential().Password
+
+    $MMARemotSetupExeURI = 'https://go.microsoft.com/fwlink/?LinkID=517476'
+    $MMASetupExe = 'MMASetup-AMD64.exe'
+    
+    $MMACommandLineArguments = 
+        '/Q /C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 AcceptEndUserLicenseAgreement=1 ' +
+        "OPINSIGHTS_WORKSPACE_ID=$($GlobalVars.WorkspaceID) " +
+        "OPINSIGHTS_WORKSPACE_KEY=$($WorkspaceKey)`""
+
+
+ Node MemberServerDev
+    {
+            xRemoteFile DownloadMicrosoftManagementAgent
+        {
+            Uri = $MMARemotSetupExeURI
+            DestinationPath = "$($SourceDir)\$($MMASetupExe)"
+            MatchSource = $False
+        }
+        xPackage InstallMicrosoftManagementAgent
+        {
+             Name = "Microsoft Monitoring Agent"
+             Path = "$($SourceDir)\$($MMASetupExE)" 
+             Arguments = $MMACommandLineArguments 
+             Ensure = 'Present'
+             InstalledCheckRegKey = 'SOFTWARE\Microsoft\Microsoft Operations Manager\3.0\Setup'
+             InstalledCheckRegValueName = 'Product'
+             InstalledCheckRegValueData = 'Microsoft Monitoring Agent'
+             ProductID = ''
+             DependsOn = "[xRemoteFile]DownloadMicrosoftManagementAgent"
+        }
+    }
+}
+
+
+

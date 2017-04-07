f0eb478269883415132d94a32c5895d020df63a5
Robert Wakefield <Robert Wakefield>
2017-04-07 09:24:02 -0500

    adding files

---
+++ DSC/lampstack.ps1
@@ -0,0 +1,51 @@
+﻿Configuration lampstack
+{
+Import-DscResource -ModuleName  nx -ModuleVersion 1.0
+Import-DscResource -ModuleName nxNetworking -ModuleVersion 1.1
+
+Node "NixWeb1.0" {
+
+nxScript checkapt {
+GetScript = @'
+#!/bin/bash
+apt-get check
+'@
+
+SetScript = @'
+#!/bin/bash
+apt-get update
+'@
+
+TestScript = @'
+#!/bin/bash
+exit 0
+'@
+}
+
+nxPackage mysql {
+    PackageManager = 'apt'
+    Ensure = 'Present'
+    Name = 'mysql-server'
+}
+
+nxPackage php {
+    PackageManager = 'apt'
+    Ensure = 'Present'
+    Name = 'php5-mysql'    
+}
+
+nxPackage Apache {
+    PackageManager = 'apt'
+    Ensure = 'Present'
+    Name = 'apache2'
+}
+
+nxFile ExampleFile {
+    DestinationPath = "/tmp/example"
+    Contents = "hello world"
+    Ensure = "Present"
+    Type = "File"
+}
+
+}
+}
---
+++ Find-NonBackupComputer.ps1
@@ -0,0 +1,78 @@
+﻿<#
+    .SYNOPSIS
+       Add a synopsis here to explain the PSScript. 
+
+    .Description
+        Give a description of the Script.
+
+#>
+Param(
+
+)
+
+Import-Module SCOrchDev-Utility -Verbose:$False
+Import-Module SCOrchDev-Exception -Verbose:$False
+Import-Module SCOrchDev-File -Verbose:$False
+Import-Module SCOrchDev-GitIntegration -Verbose:$False
+Import-Module AzureRM.Profile -Verbose:$False
+Import-Module AzureRM.RecoveryServices -Verbose:$False
+
+$ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+$CompletedParameters = Write-StartingMessage -CommandName Find-NonBackupComputer.ps1
+
+$SubscriptionName = Get-AutomationVariable -Name "SubscriptionName"
+$ResourceGroupName = Get-AutomationVariable -Name "ResourceGroupName"
+$SubscriptionTenant = Get-AutomationVariable -Name "SubscriptionTenant"
+
+Try
+{
+$connectionName = "AzureRunAsConnection"
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+
+    $ProtectedVM = @()
+    $Vault = Get-AzureRmRecoveryServicesVault
+    Foreach($_Vault in $Vault)
+    {
+        $_Vault | Set-AzureRmRecoveryServicesVaultContext
+        $Container = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM
+        Foreach($_Container in $Container)
+        {
+            $ProtectedVM += "$($_Container.FriendlyName.ToLower());$($_Container.ResourceGroupName.ToLower())"
+        }
+    }
+    
+    $VM = Get-AzureRmVM
+    Foreach($_VM in $VM)
+    {
+        if($ProtectedVM -notcontains "$($_VM.Name.ToLower());$($_VM.ResourceGroupName.ToLower())")
+        {
+            Invoke-AutomationWatcherAction -CustomProperties @{
+                'VMName' = $_VM.Name
+                'VMResourceGroup' = $_VM.ResourceGroupName
+                'Location' = $_VM.Location
+            }
+        }
+    }
+}
+Catch
+{
+    $Exception = $_
+    $ExceptionInfo = Get-ExceptionInfo -Exception $Exception
+    Switch ($ExceptionInfo.FullyQualifiedErrorId)
+    {
+        Default
+        {
+            Write-Exception $Exception -Stream Warning
+        }
+    }
+}
+
+Write-CompletedMessage @CompletedParameters

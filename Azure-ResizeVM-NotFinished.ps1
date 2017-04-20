7a260becb2f9fb37d3d769c3282c3b5a92727bec
Robert Wakefield <Robert Wakefield>
2017-04-20 00:31:52 -0500

    Adding File

---
+++ Azure-ResizeVM-NotFinished.ps1
@@ -0,0 +1,21 @@
+﻿Param
+    (   
+        [Parameter(Mandatory=$true)]
+        [String]
+        $VMName
+
+    )
+
+$ResourceGroup = Get-AutomationVariable -Name "ResourceGroupRWEUS2"
+
+# Import Minimum Required Module Version(s)
+
+    Import-Module `
+        -Name AzureRM.Compute `
+        -MinimumVersion 2.8.0
+
+# Select Azure Cloud Environment
+    $AzureEnv = (Get-AzureRmEnvironment).Name
+# Get Azure VM Object    $VM = Get-AzureRmVm -ResourceGroupName $ResourceGroup -Name $VMName# Get New Azure VM Size for scale-up or scale-down    $currentVmSize = $vm.HardwareProfile.VmSize    $vmFamily = $currentVmSize -replace '[0-9]', '*'    $newVmSize = (Get-AzureRmVMSize -Location $vm.Location).Name |        Where-Object {$_ -Like $vmFamily} |        Out-GridView `
+            -Title "Select a new VM Size ..." `
+            -PassThru    $vm.HardwareProfile.VmSize = $newVmSize    $vm | Update-AzureRmVM 

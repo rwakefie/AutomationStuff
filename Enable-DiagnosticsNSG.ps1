4e09d6405aab9f5e8d1e67b0c29ab24b3e3273ca
Robert Wakefield <rwake24@gmail.com>
2017-04-19 20:57:12 -0500

    adding files

---
+++ Enable-DiagnosticsNSG.ps1
@@ -0,0 +1,30 @@
+﻿
+$StorageAccountName = Get-AutomationVariable -Name "diagStorageAccount"
+$StorageAccountResourceGroup = Get-AutomationVariable -Name "diagResourceGroup"
+$RetentionDays = "7"
+
+try
+{
+	$ErrorActionPreference = "Stop"
+	$Error.Clear()
+
+	$StorageAccount = Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResourceGroup -Name $StorageAccountName
+	$NetworkSecurityGroups = Get-AzureRmNetworkSecurityGroup
+
+	foreach ($NetworkSecurityGroup in $NetworkSecurityGroups)
+	{
+		$DiagnosticSettings = Get-AzureRmDiagnosticSetting -ResourceId $NetworkSecurityGroup.Id
+		if ($DiagnosticSettings.StorageAccountId -eq $null)
+		{
+			if($NetworkSecurityGroup.ResourceGroupName.Contains($StorageAccountResourceGroup))
+			{
+				Set-AzureRmDiagnosticSetting -ResourceId $NetworkSecurityGroup.Id -StorageAccountId $StorageAccount.Id -Enabled $true -Categories 'NetworkSecurityGroupEvent','NetworkSecurityGroupRuleCounter' -RetentionEnabled $true -RetentionInDays $RetentionDays
+			}
+		}
+	}
+}
+catch
+{
+	Write-Output $Error
+}
+
---
+++ NSGCleanup.ps1
@@ -0,0 +1,11 @@
+﻿$StorageAccountName = $null
+$NetworkSecurityGroups = Get-AzureRmNetworkSecurityGroup
+
+	foreach ($NetworkSecurityGroup in $NetworkSecurityGroups)
+	{
+		$DiagnosticSettings = Get-AzureRmDiagnosticSetting -ResourceId $NetworkSecurityGroup.Id
+			if($NetworkSecurityGroup.ResourceGroupName.Contains($StorageAccountResourceGroup))
+			{
+				Set-AzureRmDiagnosticSetting -ResourceId $NetworkSecurityGroup.Id -Enabled $false -StorageAccountId $null
+			}
+		}

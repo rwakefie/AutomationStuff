ae7c34623c66320ce4968cdbc93beb807e4ad274
Robert Wakefield <Robert Wakefield>
2017-04-24 09:09:35 -0500

    Initial Commit

---
+++ AzureRM-TriggerAAWebhook.ps1
@@ -0,0 +1,13 @@
+﻿$webhookurl = 'https://s2events.azure-automation.net/webhooks?token=[secrettoken]'
+
+$body = @{"LastName" = "Stranger"; "FirstName" = "Stefan"}
+
+$params = @{
+    ContentType = 'application/json'
+    Headers = @{'from' = 'Stefan Stranger'; 'Date' = "$(Get-Date)"}
+    Body = ($body | convertto-json)
+    Method = 'Post'
+    URI = $webhookurl
+}
+
+Invoke-RestMethod @params -Verbose

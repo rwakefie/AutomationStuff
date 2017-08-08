fe20386718d741e32c19b9c3556c06c563432e49
Robert Wakefield <rwake24@gmail.com>
2017-08-08 12:48:55 -0500

    initial commit

---
+++ DSC/DSCAdmistratorAccount.ps1
@@ -0,0 +1,18 @@
+﻿Configuration AdministratorAccount
+{
+    #Import Modules!
+    #Import-DscResource -Module xPSDesiredStateConfiguration
+
+
+    Node AdministratorAccount-1.0
+    {
+        User Administrator
+        {
+            Ensure = "Present"  # To ensure the user account does not exist, set Ensure to "Absent"
+            UserName = "Administrator"
+            Description = "This is the local admin account"
+            Disabled = $True
+            PasswordNeverExpires = $True
+        }
+    }
+}

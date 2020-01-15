77035abdc69df3bdd2f771832baa603eb6079840
Robert Wakefield <rwake24@gmail.com>
2020-01-15 08:45:04 -0600

    Create WVDImagePrep.ps1
    Adding File

---
+++ WVDImagePrep.ps1
@@ -0,0 +1,175 @@
+﻿# This section is for OneDrive Configuration.
+# Master Script for WVD Image Prep
+# Script authors: Chris Nylen <Chris.Nylen@microsoft.com>; John Jenner <John.Jenner@microsoft.com>
+# 
+#
+#<Please run the first section for OneDrive if needed, then proceed to run the rest of the script>#
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+# Uninstall OneDrive 
+Run c:\Files\office\OneDriveSetup.exe /uninstall
+
+REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64
+
+#Install OneDrive
+Run c:\Files\office\OneDriveSetup.exe /allusers
+
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
+
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
+
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "<your-AzureAdTenantId>" /f
+
+
+
+<------------------------------Once the above is run continue to run the rest of the script---------------------------------->
+
+
+
+
+# The following steps are from: https://docs.microsoft.com/en-us/azure/virtual-desktop/set-up-customize-master-image
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
+# Set this variable to your FSLogix profile directory
+$FSLUNC = "\\my-sofs1\UpdStorage"
+
+Write-Host "This script will prepare your image for capture and eventual upload to Azure."
+
+Write-Host "Disabling Automatic Updates..."
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU /v NoAutoUpdate /t REG_DWORD /d 1 /f
+
+Write-Host "Setting OneDrive for Business policies"
+#Configure OneDrive to start at sign-in for all users
+REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files (x86)\Microsoft OneDrive\OneDrive.exe /background" /f
+#Silently configure user accounts
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
+#Redirect and move Windows known folders to OneDrive - Make sure to change the AAD ID to match your own AAD!!!! 
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "89bffd51-XXXX-XXXX-a48e-ec455955c59a" /f
+
+# Skiprearm for windows activation after sysprepping
+#Now open Regedit and go to the following key:
+
+#REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\Currentversion\SL" /v ""
+
+# Configure session timeout policies
+Write-Host "Configuring session timeout policies..."
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v RemoteAppLogoffTimeLimit /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fResetBroken /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v MaxConnectionTime /t REG_DWORD /d 10800000 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v RemoteAppLogoffTimeLimit /t REG_DWORD /d 0 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v MaxDisconnectionTime /t REG_DWORD /d 5000 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v MaxIdleTime /t REG_DWORD /d 7200000 /f
+
+# Enable timezone redirection
+Write-Host "Enabling time zone redirection..."
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" /v fEnableTimeZoneRedirection /t REG_DWORD /d 1 /f
+
+# Disable Storage Sense
+Write-Host "Disabling Storage Sense..."
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy /v 01 /t REG_DWORD /d 0 /f
+
+# The following steps are from: https://docs.microsoft.com/en-us/azure/virtual-machines/windows/prepare-for-upload-vhd-image
+
+Write-Host "Preparing image for upload to Azure..."
+
+# Remove the WinHTTP proxy
+netsh winhttp reset proxy
+
+# Set Coordinated Universal Time (UTC) time for Windows and the startup type of the Windows Time (w32time) service to Automatically
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -name "RealTimeIsUniversal" -Value 1 -Type DWord -force
+Set-Service -Name w32time -StartupType Automatic
+
+# Set the power profile to the High Performance
+powercfg /setactive SCHEME_MIN
+
+# Make sure that the environmental variables TEMP and TMP are set to their default values
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -name "TEMP" -Value "%SystemRoot%\TEMP" -Type ExpandString -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -name "TMP" -Value "%SystemRoot%\TEMP" -Type ExpandString -force
+
+# Set Windows services to defaults - This typically fails due to a permissions error, need to investigate why. May be due to differences in client vs Server os
+Set-Service -Name bfe -StartupType Automatic
+Set-Service -Name dhcp -StartupType Automatic
+Set-Service -Name dnscache -StartupType Automatic
+Set-Service -Name IKEEXT -StartupType Automatic
+Set-Service -Name iphlpsvc -StartupType Automatic
+Set-Service -Name netlogon -StartupType Manual
+Set-Service -Name netman -StartupType Manual
+Set-Service -Name nsi -StartupType Automatic
+Set-Service -Name termService -StartupType Manual
+Set-Service -Name MpsSvc -StartupType Automatic
+Set-Service -Name RemoteRegistry -StartupType Automatic
+Set-Service -Name Winrm -startuptype Automatic
+
+# Ensure RDP is enabled
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDenyTSConnections" -Value 0 -Type DWord -force
+
+# Set RDP Port to 3389 - Unnecessary for WVD due to reverse connect, but helpful for backdoor administration with a jump box 
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "PortNumber" -Value 3389 -Type DWord -force
+
+# Listener is listening on every network interface
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "LanAdapter" -Value 0 -Type DWord -force
+
+# Configure NLA
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SecurityLayer" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "fAllowSecProtocolNegotiation" -Value 1 -Type DWord -force
+
+# Set keep-alive value
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "KeepAliveEnable" -Value 1  -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "KeepAliveInterval" -Value 1  -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "KeepAliveTimeout" -Value 1 -Type DWord -force
+
+# Reconnect
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDisableAutoReconnect" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "fInheritReconnectSame" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "fReconnectSame" -Value 0 -Type DWord -force
+
+# Limit number of concurrent sessions
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "MaxInstanceCount" -Value 4294967295 -Type DWord -force
+
+# Remove any self signed certs
+Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SSLCertificateSHA1Hash" -force
+
+# Turn on Firewall
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+
+# Allow WinRM
+REG add "HKLM\SYSTEM\CurrentControlSet\services\WinRM" /v Start /t REG_DWORD /d 2 /f
+net start WinRM
+Enable-PSRemoting -force
+Set-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)" -Enabled True
+
+# Allow RDP
+Set-NetFirewallRule -DisplayGroup "Remote Desktop" -Enabled True
+
+# Enable File and Printer sharing for ping
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+
+# Add Defender exclusion for FSLogix
+Add-MpPreference -ExclusionPath $FSLUNC
+
+#Add FSLogix settings
+New-Item -Path HKLM:\Software\FSLogix\ -Name Profiles –Force
+Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "Enabled" -Type "Dword" -Value "1"
+New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "VHDLocations" -Value $FSLUNC -PropertyType MultiString -Force
+Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SizeInMBs" -Type "Dword" -Value "32768"
+Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "IsDynamic" -Type "Dword" -Value "1"
+Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "LockedRetryCount" -Type "Dword" -Value "1"
+Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "LockedRetryInterval" -Type "Dword" -Value "0"
+
+# If you want to run the script fully then uncomment the below lines (164-166) 
+
+# Launch Sysprep
+# Write-Host "We'll now launch Sysprep."
+# C:\Windows\System32\Sysprep\Sysprep.exe /generalize /oobe /shutdown
+
+<------------------------------------------------------------------------------------------->
+
+# Windows Store disable setting
+# Start the registry editor (regedit.exe).
+# New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\WindowsStore\ -Name "DisableStoreApps" -Type "Dword" -Value "1"
+
+# WVD CLient 
+# REG ADD HKLM\Software\Microsoft\MSRDC\Policies /t REG_SZ /v ReleaseRing /d insider /f
